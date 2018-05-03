cae39ca686e9752f1c3f5441cdd16f53441bf58c
CaptainFluffyToes <djkhan85@gmail.com>
2018-05-03 12:26:29 -0400

    fixed naming changed script to use bash instead of sh

--- conjur_cluster_config.sh
+++ conjur_cluster_config.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -e
+#!/bin/bash
 
 ####Variables###
 NAMESPACE=cyberark-conjur-enterprise
@@ -16,7 +16,8 @@ FOLLOWER_URL=$FOLLOWER_SERVICE_NAME.$NAMESPACE.svc.cluster.local
 AUTHNK8S_URL=$AUTHNK8S_SERVICE_NAME.$NAMESPACE.svc.cluster.local
 
 
-function main(){
+main()
+{
 	printf "\n----\n"
 	printf "\nSetting up Conjur Master\n"
 	master_config
@@ -28,7 +29,8 @@ function main(){
 	authnk8s_config
 }
 
-function master_config(){
+master_config()
+{
 	local master_pod_name=$(kubectl get pods -n $NAMESPACE -l app=conjur-node,role=master --no-headers | awk '{print $1;}')
 	local master_container_name=master
 	
@@ -38,18 +40,20 @@ function master_config(){
 	printf "\n\nMaster instance has been configured."
 }
 
-function standby_config(){
+standby_config()
+{
 	local standby_pod_names=$(kubectl get pods -n $NAMESPACE -l app=conjur-node,role=standby --no-headers | awk '{print $1;}')
 	local standby_container_name=standby
 	local master_pod_name=$(kubectl get pods -n $NAMESPACE -l app=conjur-node,role=master --no-headers | awk '{print $1;}')
 	local master_container_name=master
 
 	printf "\nGenerating standby SEED package.\n"
-	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -i -t evoke ca issue $STANDBY_URL
-	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -i -t evoke seed standby $STANDBY_URL $MASTER_URL > standby.tar
+	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -- evoke ca issue --force $STANDBY_URL
+	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name evoke seed standby $STANDBY_URL $MASTER_URL > standby.tar
 
 	for pod in $standby_pod_names; do
 		printf "\nWorking on Pod:$pod.\n"
+#		kubectl cp $NAMESPACE/$pod:/standby.tar -c $standby_container_name standby.tar 
 		kubectl cp standby.tar $NAMESPACE/$pod:/standby.tar -c $standby_container_name
 		printf "\n\nUnpacking and importing standby seed package.\n\n"
 		kubectl -n $NAMESPACE exec $pod -c $standby_container_name evoke unpack seed /standby.tar
@@ -59,15 +63,16 @@ function standby_config(){
 	rm standby.tar
 }
 
-function follower_config(){
+follower_config()
+{
 	local follower_pod_names=$(kubectl get pods -n $NAMESPACE -l app=conjur-node,role=follower --no-headers | awk '{print $1;}')
 	local follower_container_name=follower
 	local master_pod_name=$(kubectl get pods -n $NAMESPACE -l app=conjur-node,role=master --no-headers | awk '{print $1;}')
 	local master_container_name=master
 
 	printf "\nGenerating follower SEED package.\n"
-	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -i -t evoke ca issue $FOLLOWER_URL
-	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -i -t evoke seed follower $FOLLOWER_URL $MASTER_URL > follower.tar
+	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -- evoke ca issue --force $FOLLOWER_URL
+	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name evoke seed follower $FOLLOWER_URL $MASTER_URL > follower.tar
 	
 	for pod in $follower_pod_names; do
 		printf "\nWorking on Pod:$pod.\n"
@@ -80,22 +85,23 @@ function follower_config(){
 	rm follower.tar
 }
 
-function authnk8s_config(){
+authnk8s_config()
+{
 	local authnk8s_pod_names=$(kubectl get pods -n $NAMESPACE -l app=conjur-node,role=authnk8s --no-headers | awk '{print $1;}')
-	local authnk8s_container_name=authnk8s
+	local authnk8s_container_name=follower
 	local master_pod_name=$(kubectl get pods -n $NAMESPACE -l app=conjur-node,role=master --no-headers | awk '{print $1;}')
 	local master_container_name=master
 	
 	printf "\nGenerating authnk8s SEED package.\n"
-	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -i -t evoke ca issue $AUTHNK8S_URL
-	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -i -t evoke seed follower $AUTHNK8S_URL $MASTER_URL > authnk8s.tar
+	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name -- evoke ca issue --force $AUTHNK8S_URL
+	kubectl -n $NAMESPACE exec $master_pod_name -c $master_container_name evoke seed follower $AUTHNK8S_URL $MASTER_URL > authnk8s.tar
 	
 	for pod in $authnk8s_pod_names; do
 		printf "\nWorking on Pod:$pod\n"
-		kubectl cp authnk8s.tar $NAMESPACE/$pod:/authnk8s.tar -c $follower_container_name
+		kubectl cp authnk8s.tar $NAMESPACE/$pod:/authnk8s.tar -c $authnk8s_container_name
 		printf "\n\nUnpacking and importing authnk8s seed package.\n\n"
-		kubectl -n $NAMESPACE exec $pod -c $follower_container_name evoke unpack seed /authnk8s.tar
-		kubectl -n $NAMESPACE exec $pod -c $follower_container_name evoke configure follower
+		kubectl -n $NAMESPACE exec $pod -c $authnk8s_container_name evoke unpack seed /authnk8s.tar
+		kubectl -n $NAMESPACE exec $pod -c $authnk8s_container_name evoke configure follower
 	done
 	
 	rm authnk8s.tar
